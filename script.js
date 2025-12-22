5cb333904bec9781012d545ec7005882e456ceb5
drex109 <96746274+drex109@users.noreply.github.com>
2025-12-22 14:00:11 -0700

    created function for adding book to library

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const myLibrary = [];
+
 function Book(name, author, pageCount) {
     this.name = name;
     this.author = author;
     this.pageCount = pageCount;
+    this.id = crypto.randomUUID();
 }
 
-let hobbit = new Book("The Hobbit", "JRR Tolkien", "350");
-console.log(hobbit);
+function addBookToLibrary(name, author, pageCount) {
+    let book = new Book(name, author, pageCount);
+    console.log(book);
+    myLibrary.push(book);
+    
+}
+
+addBookToLibrary('The Hobbit', 'JRR Tolkien', '450');
+console.log(myLibrary)
+
+// let hobbit = new Book("The Hobbit", "JRR Tolkien", "350");
+// console.log(hobbit);
