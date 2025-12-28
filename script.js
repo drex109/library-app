7f7efd2256cf8189e471c438719a5274832860bd
drex109 <96746274+drex109@users.noreply.github.com>
2025-12-27 21:32:51 -0700

    began working on displayBooks function, styling cards for clarity during dev

--- script.js
+++ script.js
@@ -17,5 +17,37 @@ function addBookToLibrary(name, author, pageCount) {
 addBookToLibrary('The Hobbit', 'JRR Tolkien', '450');
 console.log(myLibrary)
 
+let books = document.querySelector('.books');
+
+function displayBooks(){
+    for(let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement('div');
+        book.classList.add('book');
+        books.appendChild(book);
+
+        const title = document.createElement('h2');
+        title.classList.add('title');
+        title.textContent = `Title: ${myLibrary[i].name}`;
+        book.appendChild(title);
+
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.textContent = `Author: ${myLibrary[i].author}`;
+        book.appendChild(author);
+
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        pages.textContent = `Page Count: ${myLibrary[i].pageCount}`;
+        book.appendChild(pages);
+
+        const idCode = document.createElement('p')
+        idCode.classList.add('id');
+        idCode.textContent = myLibrary[i].id;
+        book.appendChild(idCode);
+    }
+}
+
+displayBooks();
+
 // let hobbit = new Book("The Hobbit", "JRR Tolkien", "350");
 // console.log(hobbit);
